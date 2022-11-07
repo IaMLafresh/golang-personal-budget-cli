dc0806768e0853e5dfd8e3b662bf9f1eba7e8c36
IaMLafresh <mglabreche@gmail.com>
2022-11-07 10:59:30 -0500

    Attempt 1

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -3,3 +3,9 @@ package module1
 // Budget stores budget information
 
 // Item stores item information
+type Item struct {
+	Max         float32
+	Items       []Item
+	Description string
+	Price       float32
+}
